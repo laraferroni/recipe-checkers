ffa737c294d1e21a3311d8eccfac8b5598609925
Lara Ferroni <lara@laraferroni.com>
2016-02-13 16:40:28 -0800

    fix default domain in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.default_url_options = { :host => 'recipecheckers.com'}
+  config.action_mailer.default_url_options = { :host => 'www.recipecheckers.com'}
   config.action_mailer.asset_host = 'http://www.recipecheckers.com/'
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
